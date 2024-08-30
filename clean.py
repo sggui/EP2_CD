0df248c72740dd2b69b89cef7ab08d3183379627
Gustavo Neumann <gneumann7164@gmail.com>
2024-08-30 16:02:03 -0300

    remocao de habilidades duplicadas

--- clean.py
+++ clean.py
@@ -10,11 +10,10 @@ df = pd.DataFrame(data)
 def convert_height(height):
     height_clean = re.sub(r'\(.*?\)', '', height).strip()
     height_clean = re.sub(r'[^\d.]+', '', height_clean)
-
     try:
         meters = float(height_clean)
     except ValueError:
-        meters = 0  
+        meters = 0
     centimeters = meters * 100
     return f"{centimeters:.1f} cm"
 
@@ -23,18 +22,25 @@ def convert_weight(weight):
     weight_clean = weight_clean.replace('Â', '').replace('\u00a0', ' ')
     return weight_clean
 
+def remove_duplicates(items):
+    seen = set()
+    result = []
+    for item in items:
+        item_tuple = tuple(item.items())
+        if item_tuple not in seen:
+            seen.add(item_tuple)
+            result.append(item)
+    return result
 
 df['height_cm'] = df['height_cm'].apply(convert_height)
 df['weight_kg'] = df['weight_kg'].apply(convert_weight)
-
+df['abilities'] = df['abilities'].apply(remove_duplicates)
 df['number'] = pd.to_numeric(df['number'])
-
 df = df.sort_values(by='number')
 
 print(df)
 
 df.to_csv('clean_pokemon.csv', index=False)
-
 data_list = df.to_dict(orient='records')
 
 with open('clean_pokemon.json', 'w', encoding='utf-8') as f:
